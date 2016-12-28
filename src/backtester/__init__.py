411592b7b7e846092bbaf92c23f8c1061adc6789
junorouse <junorouse@gmail.com>
2016-12-28 21:52:28 +0900

    Fixing Robert's bug.

--- src/backtester/__init__.py
+++ src/backtester/__init__.py
@@ -1,9 +1,13 @@
 from flask import abort, Flask, g, render_template, request
 
 from backtester.config import configure_app
+from backtester.main.controllers import main
 
 
 app = Flask(__name__,
             template_folder='templates')
 
 configure_app(app)
+
+
+app.register_blueprint(main, url_prefix='/')
