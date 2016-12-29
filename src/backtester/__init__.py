0b2e639dcdc3b2280b2c7c258056126ea8e70f3b
junorouse <junorouse@gmail.com>
2016-12-29 09:49:57 +0900

    Derp

--- src/backtester/__init__.py
+++ src/backtester/__init__.py
@@ -1,4 +1,6 @@
 from flask import abort, Flask, g, render_template, request
+from flask_security import current_user
+from flask_babel import Babel
 
 from backtester.config import configure_app
 from backtester.main.controllers import main
@@ -7,7 +9,31 @@ from backtester.main.controllers import main
 app = Flask(__name__,
             template_folder='templates')
 
+babel = Babel(app)
 configure_app(app)
+app.jinja_env.add_extension('jinja2.ext.loopcontrols')
 
+@babel.localeselector
+def get_locale():
+    return g.get('lang_code', app.config['BABEL_DEFAULT_LOCALE'])
 
-app.register_blueprint(main, url_prefix='/')
+@babel.timezoneselector
+def get_timezone():
+    user = g.get('user', None)
+    if user is not None:
+        return user.timezone
+
+@app.errorhandler(404)
+def page_not_found(error):
+    app.logger.error('Page not found: %s', (request.path, error))
+    return render_template('404.html'), 404
+
+@app.context_processor
+def inject_data():
+    return dict(user=current_user,)
+
+@app.route('/')
+def index(lang_code=None):
+    return render_template('index.html')
+
+app.register_blueprint(main, url_prefix='/main')
--- src/backtester/main/controllers.py
+++ src/backtester/main/controllers.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, current_app, render_template
 
+from backtester.main.views import Main
+
 main = Blueprint('main', __name__, template_folder='templates')
 
-@main.route('//')
+@main.route('/')
 def display_index():
     return render_template("index.html")
